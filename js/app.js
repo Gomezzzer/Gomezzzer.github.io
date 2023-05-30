b0e57392cc64e834479bc254885bf695fc09d761
Nicoli1977 <86806874+Gomezzzer@users.noreply.github.com>
2023-05-30 17:01:34 -0500

    new portfolio tab view

--- js/app.js
+++ js/app.js
@@ -1,57 +1,4 @@
-// mobile menu
-/*const btn = document.querySelector("button.mobile-menu-button");*/
-const menu = document.querySelector(".menu");
 
-
-
-	// select dom items
-	const menuBtn =
-		document.querySelector(".menu-btn");
-
-	const menuNav =
-		document.querySelector(".menu-nav");
-
-	const menuBranding =
-		document.querySelector(".menu-branding");
-
-	const navItems =
-		document.querySelectorAll(".nav-item");
-
-	// Set the initial state of the menu
-	let showMenu = false;
-
-	menuBtn.addEventListener("click", toggleMenu);
-
-	function toggleMenu() {
-		if (!showMenu) {
-			menuBtn.classList.add("close");
-			menu.classList.add("show");
-			menuNav.classList.add("show");
-			menuBranding.classList.add("show");
-			navItems.forEach((item) =>
-				item.classList.add("show"));
-
-			// Reset the menu state
-			showMenu = true;
-		} else {
-			menuBtn.classList.remove("close");
-			menu.classList.remove("show");
-			menuNav.classList.remove("show");
-			menuBranding.classList.remove("show");
-			navItems.forEach((item) =>
-				item.classList.remove("show"));
-
-			// Reset the menu state
-			showMenu = false;
-		}	
-	}
-
-	menuNav.addEventListener("click", toggleMenu, false);
-	var init = function() {
-		 bindActions();
-		  menuNavClicked();
-  
-  };
 
   // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
@@ -64,9 +11,24 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
-   
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
 
+burger.addEventListener("click", () => {
+	ul.classList.toggle("show");
+  });  
 
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
 	
 	
 
