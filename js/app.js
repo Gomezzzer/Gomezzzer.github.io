f96c29be9838fc497445145692c8284602e0db93
Nicoli1977 <86806874+Gomezzzer@users.noreply.github.com>
2023-06-06 23:45:33 -0500

    margin desktop

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-      icon.src= "images/sun.png";
+      icon.src= "images/egg.png";
     }else{
       icon.src = "images/moon.png";
     }
